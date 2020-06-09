d5f297754db827f86bfb4897bcc4805ffb5244f8
Patrick Furtak <patrick@patrickfurtak.com>
2020-06-09 13:51:28 -0400

    Reviewed indexing / slicing with colon notation

---
+++ fundamentals/indexing.py
@@ -0,0 +1,23 @@
+my_string = "hello world"
+
+# indexing
+print(my_string)
+print(my_string[2])
+print(my_string[7])
+print(my_string[-1])
+print(my_string[-3])
+
+# slicing
+print(my_string[2:])
+print(my_string[:3])
+print(my_string[3:6])
+print(my_string[6:])
+print(my_string[:5])
+
+# slicing with step size
+print(my_string[::2])
+print(my_string[:5:2])
+print(my_string[:-3:4])
+
+# reverse string trick with slicing
+print(my_string[::-1])
