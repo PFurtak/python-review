1901077deb8b14c75d69c1cfa8e3931b8135609e
Patrick Furtak <patrick@patrickfurtak.com>
2020-06-10 02:48:06 -0400

    Reviewed string methods

---
+++ fundamentals/stringMethods.py
@@ -0,0 +1,24 @@
+name = "Sam"
+
+# name[0] = "P"
+
+last_letters = name[1:]
+
+'P' + last_letters
+
+x = "Hello World"
+
+x + ", Birds are chirping"
+
+letter = 'z'
+
+print(letter * 10)
+
+x = x.upper()
+print(x)
+
+x = x.lower()
+print(x)
+
+x = x.split('o')
+print(x)
